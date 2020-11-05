aa9e1ddaa547c7f1b4699c8959a04ebf20443293
Josh Ye <joshua.ye@duke.edu>
2020-11-05 12:13:45 -0500

    test portal block

--- src/main/java/net/mcreator/utilitymod/ClientInit.java
+++ src/main/java/net/mcreator/utilitymod/ClientInit.java
@@ -12,13 +12,17 @@
 */
 package net.mcreator.utilitymod;
 
+import net.minecraft.client.render.RenderLayer;
+
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ClientInit implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		BlockRenderLayerMap.INSTANCE.putBlock(UtilitymodMod.TestPortalBlock_BLOCK, RenderLayer.getCutoutMipped());
 		HudRenderCallback.EVENT.register((matrices, tickDelta) -> {
 		});
 		ClientTickEvents.END_CLIENT_TICK.register((client) -> {
--- src/main/java/net/mcreator/utilitymod/UtilitymodMod.java
+++ src/main/java/net/mcreator/utilitymod/UtilitymodMod.java
@@ -15,13 +15,23 @@ package net.mcreator.utilitymod;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
+import net.minecraft.util.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.Block;
+
+import net.mcreator.utilitymod.block.TestPortalBlockBlock;
 
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.api.ModInitializer;
 
 public class UtilitymodMod implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger();
+	public static final Block TestPortalBlock_BLOCK = Registry.register(Registry.BLOCK, id("test_portal_block"), new TestPortalBlockBlock());
+	public static final BlockItem TestPortalBlock_ITEM = Registry.register(Registry.ITEM, id("test_portal_block"),
+			new BlockItem(TestPortalBlock_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 	public void onInitialize() {
 		LOGGER.info("Initializing UtilitymodMod");
 		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
